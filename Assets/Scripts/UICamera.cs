8160a01df77eef13ba695f1f7f0dad1932adf05d
Dominic Z <dmnzhang@gmail.com>
2026-08-02 22:37:32 -0400

    ui camera bounds

--- Assets/Scripts/UICamera.cs
+++ Assets/Scripts/UICamera.cs
@@ -7,22 +7,54 @@ public class UICamera : MonoBehaviour, IScrollHandler, IDragHandler {
     [SerializeField] private float zoomSpeed = 0.1f;
     [SerializeField] private float minZoom = 0.5f;
     [SerializeField] private float maxZoom = 3.0f;
+    [SerializeField] private RectTransform bounds;
 
     void Awake() {
         rectTransform = GetComponent<RectTransform>();
     }
 
     public void OnScroll(PointerEventData eventData) {
-        Vector3 newScale = rectTransform.localScale + Vector3.one * (eventData.scrollDelta.y * zoomSpeed);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out Vector2 localMousePos);
 
+        float zoomDelta = eventData.scrollDelta.y * zoomSpeed;
+        Vector3 previousScale = rectTransform.localScale;
+        Vector3 newScale = previousScale + Vector3.one * zoomDelta;
+        
         newScale.x = Mathf.Clamp(newScale.x, minZoom, maxZoom);
         newScale.y = Mathf.Clamp(newScale.y, minZoom, maxZoom);
         newScale.z = 1f;
 
         rectTransform.localScale = newScale;
+
+        Vector3 deltaScale = newScale - previousScale;
+        rectTransform.localPosition -= Vector3.Scale(localMousePos, deltaScale);
+
+        KeepInBounds();
     }
 
     public void OnDrag(PointerEventData eventData) {
         rectTransform.anchoredPosition += eventData.delta;
+
+        KeepInBounds();
+    }
+
+    private void KeepInBounds() {
+        RectTransform parentRect = rectTransform.parent as RectTransform;
+        Vector2 viewSize = parentRect.rect.size;
+        Vector2 panelSize = Vector2.Scale(bounds.rect.size, rectTransform.localScale);
+
+        float minX = (viewSize.x - panelSize.x) * 0.5f;
+        float maxX = (panelSize.x - viewSize.x) * 0.5f;
+        float minY = (viewSize.y - panelSize.y) * 0.5f;
+        float maxY = (panelSize.y - viewSize.y) * 0.5f;
+
+        if (panelSize.x < viewSize.x) { minX = 0; maxX = 0; }
+        if (panelSize.y < viewSize.y) { minY = 0; maxY = 0; }
+
+        Vector3 currentPos = rectTransform.localPosition;
+        currentPos.x = Mathf.Clamp(currentPos.x, minX, maxX);
+        currentPos.y = Mathf.Clamp(currentPos.y, minY, maxY);
+
+        rectTransform.localPosition = currentPos;
     }
 }
